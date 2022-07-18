0d0651783bbe522e2844e110607580be48efe5c9
FunkySphere <krauzowiczs42@gmail.com>
2022-07-18 09:42:45 +0200

    Sound testing functionality

--- src/components/modalComponents/Settings.vue
+++ src/components/modalComponents/Settings.vue
@@ -11,31 +11,32 @@
     <input type="number" v-model="shortLength" min="1" />
     <p>Long Break length (minutes):</p>
     <input type="number" v-model="longLength" min="1" />
-    <br>
-    <p>Sound select:
-    <select v-model="sound">
-      <option value=1>
-        Bell 1
-      </option>
-      <option value=2>
-        Bell 2
-      </option>
-      <option value=3>
-        Bell 3
-      </option>
-      <option value=4>
-        Bell 4
-      </option>
-    </select></p>
+    <br />
+    <p>
+      Sound select:
+      <select v-model="sound">
+        <option value="1">Bell 1</option>
+        <option value="2">Bell 2</option>
+        <option value="3">Bell 3</option>
+        <option value="4">Bell 4</option>
+      </select>
+    </p>
     <p>
       Sound volume:
-      <input type="range" min="1" max="100" class="slider" v-model="soundVolume">
-      {{soundVolume}}
+      <input
+        type="range"
+        min="1"
+        max="100"
+        class="slider"
+        v-model="soundVolume"
+      />
+      {{ soundVolume }}
     </p>
 
     <br />
     <button @click="saveSettingsAndClose">OK</button>
     <button @click="restoreDefault">Restore Default</button>
+    <button @click="testSound">Test Sound</button>
   </div>
 </template>
 
@@ -48,10 +49,28 @@ export default {
       shortLength: null,
       longLength: null,
       sound: null,
-      soundVolume: null
+      soundVolume: null,
     };
   },
   methods: {
+    testSound() {
+      if (this.sound) {
+        var soundToTest;
+        if (this.sound == 1) {
+          soundToTest = new Audio(require("../../assets/bell1.flac"));
+        } else if (this.sound == 2) {
+          soundToTest = new Audio(require("../../assets/bell2.wav"));
+        } else if (this.sound == 3) {
+          soundToTest = new Audio(require("../../assets/bell3.wav"));
+        } else if (this.sound == 4) {
+          soundToTest = new Audio(require("../../assets/bell4.wav"));
+        }
+        if (this.soundVolume) {
+          soundToTest.volume = this.soundVolume / 100;
+        }
+        soundToTest.play();
+      }
+    },
 
     restoreDefault() {
       this.$emit("settings", 25, 5, 20, false, 1, 0.75);
